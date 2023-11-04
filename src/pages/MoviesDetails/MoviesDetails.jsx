76031e8bf98d315c83ffd40df84dccd8baf36ddb
MykhailoMamin <mishamamin2016@gmail.com>
2023-11-04 12:21:08 +0200

    commit

--- src/pages/MoviesDetails/MoviesDetails.jsx
+++ src/pages/MoviesDetails/MoviesDetails.jsx
@@ -41,9 +41,7 @@ const MoviesDetails = () => {
   return (
     <>
       <GoBackLink>
-        <Link to={backLinkHref}>
-          Go back<span>.</span>
-        </Link>
+        <Link to={backLinkHref}>Go back</Link>
       </GoBackLink>
       <FilmWrapper>
         <FilmImg
@@ -72,12 +70,12 @@ const MoviesDetails = () => {
         <StyledList>
           <ListItem>
             <NavLink to="cast" state={location.state}>
-              Cast<span>.</span>
+              Cast
             </NavLink>
           </ListItem>
           <ListItem>
             <NavLink to="reviews" state={location.state}>
-              Reviews<span>.</span>
+              Reviews
             </NavLink>
           </ListItem>
         </StyledList>
